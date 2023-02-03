62a55f0267a0d1712977d3c24e3c555e8674684f
sean-madu <madusean2@gmail.com>
2023-02-02 19:15:29 -0700

    Add Star

--- CMPUT301W23T30/app/src/main/java/com/example/cmput301w23t30/Shape.java
+++ CMPUT301W23T30/app/src/main/java/com/example/cmput301w23t30/Shape.java
@@ -1,2 +1,11 @@
-package com.example.cmput301w23t30;public class Shape {
+package com.example.cmput301w23t30;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    Shape(int x, int y ){
+        this.x = x;
+        this.y = y;
+    }
 }
